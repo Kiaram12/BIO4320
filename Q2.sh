07daae8d1b13a57c3ee16825f96e9a75fcf484f5
Kiaram12 <k_rojas@Kiaramrojas.>
2025-09-02 19:46:17 -0600

    Exercise2

--- Q2.sh
+++ Q2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "==========================================="
+echo "        System Information Report          "
+echo "==========================================="
 echo "The current user is $(whoami)"
 echo "Hostname: $(hostname)"
 echo "Date and Time: $(date)"
@@ -6,3 +10,4 @@ echo "Current Working Directory: $(pwd)"
 echo "Number of Logged-in Users: $(who | wc -l)"
 echo "System Uptime: $(uptime -p)"
 
+echo "===========================================" 
