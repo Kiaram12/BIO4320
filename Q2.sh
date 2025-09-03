36141617442b9913efa848fee14f7ebe527d8bb5
Kiaram12 <k_rojas@Kiaramrojas.>
2025-09-02 19:39:00 -0600

    Exercise 2

---
+++ Q2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "The current user is $(whoami)"
+echo "Hostname: $(hostname)"
+echo "Date and Time: $(date)"
+echo "Current Working Directory: $(pwd)"
+echo "Number of Logged-in Users: $(who | wc -l)"
+echo "System Uptime: $(uptime -p)"
+
